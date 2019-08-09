c868cf1d46fcf3f15a9861a5526c7e13ebfe4dd4
chao <chaoli.job@gmail.com>
2019-08-09 05:44:28 +0200

    removed the __init__.py from tests

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-"""for unit test, get all files start with tests"""
-import unittest
-
-def get_tests():
-    """get all files start with tests"""
-    test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('tests', pattern='test_*.py')
-    return test_suite
