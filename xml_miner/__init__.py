07d28884539bb5d71e2de65376c7a3ea7b36db4c
chao <chaoli.job@gmail.com>
2019-08-22 04:25:03 +0200

    updated the logging module to avoid double printing

--- xml_miner/__init__.py
+++ xml_miner/__init__.py
@@ -13,14 +13,14 @@ name = "xml-miner"
 
 def define_logger(mod_name):
     """Set the default logging configuration"""
-
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setFormatter(logging.Formatter(
-        '%(levelname).1s [%(asctime)s] [%(name)s] %(message)s'))
-
     logger = logging.getLogger(mod_name)
-    logger.addHandler(console_handler)
+    if not logger.handlers:
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setFormatter(logging.Formatter(
+            '%(levelname).1s [%(asctime)s] [%(name)s] %(message)s'))
+        logger.addHandler(console_handler)
     logger.setLevel(logging.INFO)
+    logger.propagate = False
     return logger
 
 
